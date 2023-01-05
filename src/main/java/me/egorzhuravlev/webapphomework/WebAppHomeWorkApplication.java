e72813a19b6639cafb23bf22e2238c6dd144d617
Icedragal <icedragal@gmail.com>
2023-01-05 15:46:17 +0300

    Initial commit

---
+++ src/main/java/me/egorzhuravlev/webapphomework/WebAppHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.webapphomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppHomeWorkApplication.class, args);
+    }
+
+}
